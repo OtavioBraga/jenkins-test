b6e516decf5a34081072eae6c7e59e52ee93283b
Otavio Braga <otavio.braga@dexatec.com.br>
2017-05-02 14:38:07 -0400

    Add project files

---
+++ test_core.py
@@ -0,0 +1,19 @@
+import unittest
+from app import meu_web_app
+
+
+class TestHome(unittest.TestCase):
+
+    def setUp(self):
+        app = meu_web_app.test_client()
+        self.response = app.get('/')
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_html_string_response(self):
+        self.assertEqual("ok", self.response.data.decode('utf-8'))
+
+    def test_content_type(self):
+        self.assertIn('text/html', self.response.content_type)
+
