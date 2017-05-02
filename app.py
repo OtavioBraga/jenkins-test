8c8d80b454caa6d4bb619cfad0bc60b4efabea43
Otavio Braga <otavio.braga@dexatec.com.br>
2017-05-02 15:40:32 -0400

    New endpoint

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
-
+import datetime
 meu_web_app = Flask('meu_web_app')
 
 
 @meu_web_app.route('/')
 def pagina_inicial():
     return 'ok'
+
+
+@meu_web_app.route('/data')
+def date():
+    return datetime.datetime.now()
