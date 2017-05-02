1c235291d2d124e8c4f65a39de46693275840e63
Otavio Braga <otavio.braga@dexatec.com.br>
2017-05-02 14:50:57 -0400

    Commit corrigido

--- test_core.py
+++ test_core.py
@@ -12,7 +12,7 @@ class TestHome(unittest.TestCase):
         self.assertEqual(200, self.response.status_code)
 
     def test_html_string_response(self):
-        self.assertEqual("ook", self.response.data.decode('utf-8'))
+        self.assertEqual("ok", self.response.data.decode('utf-8'))
 
     def test_content_type(self):
         self.assertIn('text/html', self.response.content_type)
